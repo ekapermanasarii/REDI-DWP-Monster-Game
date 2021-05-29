3fec18672e1455c91093eb92a64365bb1dbe6e00
ekapermanasarii <83867234+ekapermanasarii@users.noreply.github.com>
2021-05-29 13:21:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,62 +2,66 @@ def my_function(first_name):
     print("hi, this game is called " + first_name)
 my_function("Monster game")
 
-player = {'name': 'player 1 Eka', 'attack': 10, 'heal': 15, 'health': 100}
-print(player)
 
-monster = {'name': 'Monster Bobi', 'attack': 13, 'heal': 17, 'health': 100}
-print(monster)
+game_running =  True
 
-print('Welcome to Monster Game!!')
-print('Please select action')
-print('1) Attack')
-print('2) Heal')
+while game_running == True:
+    new_round = True
 
-player_choice = (input("insert the type of action"))
-if player_choice == '1':
-    print ('Attack')
-if player_choice == '2':
-    print ('Heal')
-elif player_choice == '3':
-    print ('Exit')
-else:
-    print ('not working')
+    import random
+    player = {'name': 'player 1 Eka', 'attack': 10, 'heal': 15, 'health': 100}
+    print(player)
 
-while monster ['health']>= 0 or player ['health']>=0:
-    if player_choice ==  "1":
-        monster['health'] = monster ['health'] - player ['attack']
-        print (monster['health'])
+    monster = {'name': 'Monster Bobi', 'attack': 13, 'heal': 17, 'health': 100}
+    print(monster)
 
-        player['health'] = player ['health'] - monster ['attack']
-        print (player ['health'])
+    print('-----' *3)
+    print('Enter Player name')
+    player['name'] = input ()
 
-    if player_choice ==  "2":
-        player['health'] = player['health'] + player['heal']
-        print(player['health'])
+    #print(player['name'] + ' has ' +  player ['health'])
 
-        monster['health'] = monster ['health'] + monster ['heal']
-        print (monster['health'])
+    while new_round == True:
 
-    elif player_choice == '2':
-        print ('Heal player')
-    else:
-        print ('not working')
+        print('Welcome to Monster Game!!')
+        print('Please select action')
+        print('1) Attack')
+        print('2) Heal')
 
-    if player ['health'] <=0:
-        print ('you lost the game')
+        while monster ['health']>= 0 and player ['health']>=0:
+            player_choice = (input("insert the type of action"))
+            if player_choice ==  "1":
+                player_attack = random.randint(10, 15)
+                monster['health'] = monster ['health'] - player ['attack']
+                print ('Monster Health:' + str(monster['health']))
 
-    if monster ['health'] <=0:
-        print ('you win!')
+                monster_attack=random.randint(11,15)
+                player['health'] = player ['health'] - monster ['attack']
+                print ('Player health:' + str(player ['health']))
 
-#while monster ['health']>=0:
-    #player_choice = (input("insert the type of action"))
+                if player_attack > monster_attack:
+                    print('You defeat the Monster')
+                elif monster_attack > player_attack:
+                    print ('Monster defeats you')
+                else:
+                    print('you lost the same amount')
 
-    #if player_choice == "1":
-        #monster['health'] = monster['health'] - player['attack']
-        #player['health'] = player['health'] - monster['attack']
-        #print(monster['health'])
-        #print(player['health'])
-    #elif player_choice == '2':
-        #print('Heal player')
-    #else:
-        #print('not working')
+                print('The Monster lost' + str(player_attack) + 'points with your attack')
+                print('The monster attacked you back and you lost' + str(monster_attack) + 'points')
+
+
+            if player_choice ==  "2":
+                monster_health = random.randint(15,25)
+                monster['health'] = monster ['health'] + monster ['heal']
+                print ('Monster Health:' + str(monster['health']))
+
+                player['health'] = player['health'] + player['heal']
+                print('Player health:' + str(player['health']))
+
+            else:
+                print ('valid input is only 1 or 2')
+
+
+        if player['health'] <= 0 or monster['health'] <= 0:
+            new_round = False
+            game_running = False
