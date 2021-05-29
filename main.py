ecaf9eb8b69545e13c5e3cc6e17773a3fbd0b318
ekapermanasarii <83867234+ekapermanasarii@users.noreply.github.com>
2021-05-29 13:42:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ def my_function(first_name):
     print("hi, this game is called " + first_name)
 my_function("Monster game")
 
-
+print('-----' * 3)
 game_running =  True
 
 while game_running == True:
     new_round = True
 
     import random
+    print("WE START A NEW GAME!")
     player = {'name': 'player 1 Eka', 'attack': 10, 'heal': 15, 'health': 100}
     print(player)
 
@@ -19,8 +20,8 @@ while game_running == True:
     print('Enter Player name')
     player['name'] = input ()
 
-    #print(player['name'] + ' has ' +  player ['health'])
-
+    print(player['name'] + ' has: ' +  str(player ['health']) + ' health')
+    print(monster['name'] + ' has: ' + str(monster['health']) + ' health')
     while new_round == True:
 
         print('Welcome to Monster Game!!')
@@ -33,11 +34,11 @@ while game_running == True:
             if player_choice ==  "1":
                 player_attack = random.randint(10, 15)
                 monster['health'] = monster ['health'] - player ['attack']
-                print ('Monster Health:' + str(monster['health']))
+                print ('Monster Health: ' + str(monster['health']))
 
                 monster_attack=random.randint(11,15)
                 player['health'] = player ['health'] - monster ['attack']
-                print ('Player health:' + str(player ['health']))
+                print ('Player health: ' + str(player ['health']))
 
                 if player_attack > monster_attack:
                     print('You defeat the Monster')
@@ -46,22 +47,44 @@ while game_running == True:
                 else:
                     print('you lost the same amount')
 
-                print('The Monster lost' + str(player_attack) + 'points with your attack')
-                print('The monster attacked you back and you lost' + str(monster_attack) + 'points')
+                print('The Monster lost ' + str(player_attack) + ' points with your attack')
+                print('The monster attacked you back and you lost ' + str(monster_attack) + ' points')
 
 
             if player_choice ==  "2":
                 monster_health = random.randint(15,25)
                 monster['health'] = monster ['health'] + monster ['heal']
-                print ('Monster Health:' + str(monster['health']))
+                print ('Monster Health: ' + str(monster['health']))
 
                 player['health'] = player['health'] + player['heal']
-                print('Player health:' + str(player['health']))
+                print('Player health: ' + str(player['health']))
 
             else:
                 print ('valid input is only 1 or 2')
 
 
         if player['health'] <= 0 or monster['health'] <= 0:
+            print ('GAME ENDS!!')
             new_round = False
-            game_running = False
+            print('-----' * 3)
+            #game_running = False
+
+    #if player_attack == randint(10, 20)
+            #monster_attack == randint(15, 25)
+                #print("player has attacked:", player_attack)
+                #print("monster has attacked:", monster_attack)
+
+# keep record: use appendy
+
+#while monster ['health']>=0:
+    #player_choice = (input("insert the type of action"))
+
+    #if player_choice == "1":
+        #monster['health'] = monster['health'] - player['attack']
+        #player['health'] = player['health'] - monster['attack']
+        #print(monster['health'])
+        #print(player['health'])
+    #elif player_choice == '2':
+        #print('Heal player')
+    #else:
+        #print('not working')
